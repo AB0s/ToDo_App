8871e390287d1fd53df2af5b0e3af40333fad552
AB0s <93539709+AB0s@users.noreply.github.com>
2023-03-01 17:45:57 +0600

    1

---
+++ lib/screens/profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import '../model/todo.dart';
+import '../constants/colors.dart';
+import '../widgets/todo_item.dart';
+
+
+
+class Profile extends StatelessWidget {
+  const Profile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
